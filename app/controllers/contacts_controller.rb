9c42335fc59cda73c87df879f08d09a67932e8cf
Artem Korotchenko <clifredd@gmail.com>
2018-04-13 17:49:38 +0500

    Finished with controller and form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,20 @@ class ContactsController < ApplicationController
   end
 
   def create
+    contact = Contact.new(contact_params)
+
+    if contact.save
+      redirect_to root_path
+      flash[:notice] = 'Message sent! Thank you.'
+    else
+      redirect_back(fallback_location: request.referer)
+      flash[:alert] = contact.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :phone, :message)
   end
 end
