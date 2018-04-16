cf60cad9f9ced23e4838bd75885140fa347c38c7
Artem Korotchenko <clifredd@gmail.com>
2018-04-16 10:48:19 +0500

    Modified production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #config.action_mailer.default_url_options = { host: '' }
+  config.action_mailer.default_url_options = { host: 'freelancer-contact-form.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default charset: 'utf-8'
